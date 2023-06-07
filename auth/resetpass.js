575db46f5e31ced1ce0f7d4b5b8bae8abaeccce6
alifmaulidanar <alifdanar1145@gmail.com>
2023-06-08 00:27:13 +0700

    Fix bug in reset-password

--- auth/resetpass.js
+++ auth/resetpass.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const Joi = require('joi');
 const admin = require('firebase-admin');
 const {JWT_SECRET} = require('./auth');
 const jwt = require('jsonwebtoken');
@@ -10,40 +11,66 @@ const userLog = require('../log/logger');
 
 const db = admin.firestore();
 
+// Validate user input using Joi
+const validateUserInput = (data) => {
+  const schema = Joi.object({
+    resetToken: Joi.string()
+      .trim()
+      .message("Token should not contain spaces")
+      .required(),
+    newPassword: Joi.string()
+      .min(8)
+      .pattern(/^\S*$/)
+      .message("Password should not contain spaces")
+      .required()
+  });
+
+  return schema.validate(data);
+};
+
 // Route untuk Reset Password
 router.post('/', async (req, res) => {
   const { resetToken, newPassword } = req.body;
-    try {
-      const decoded = await verify(resetToken, JWT_SECRET);
-      const userId = decoded.userId;
+  
+  // Validate user input
+  const { error } = validateUserInput(req.body);
+  if (error) {
+    return res.status(400).json({
+      error: error.details[0].message
+    });
+  }
 
-      const userDoc = await db.collection('users').doc(userId).get();
-      const userData = userDoc.data()
-  
-      // Hash password yang baru
-      const hashedPassword = await bcrypt.hash(newPassword, 10);
-  
-      // Memperbarui password di Firestore
-      await db.collection('users').doc(userId).update({
-        password: hashedPassword
-      });
+  try {
+    const decoded = await verify(resetToken, JWT_SECRET);
+    const userId = decoded.userId;
 
-      // Logging log messages to userLog
-    userLog.info('PASSWORD RESET', { userId, username: userData.username, email: userData.email, phone: userData.phone, address: userData.address });
-  
-      return res.status(200).json({
-        error: false,
-        message: "Password has been reset",
-        userId: userId,
-        username: userData.username,
-        email: userData.email
-      });
-    } catch (error) {
-      return res.status(500).json({
-        error: error,
-        message: "Failed to reset password"
-      });
-    }
+    const userDoc = await db.collection('users').doc(userId).get();
+    const userData = userDoc.data()
+
+    // Hash password yang baru
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    // Memperbarui password di Firestore
+    await db.collection('users').doc(userId).update({
+      password: hashedPassword
+    });
+
+    // Logging log messages to userLog
+  userLog.info('PASSWORD RESET', { userId, username: userData.username, email: userData.email, phone: userData.phone, address: userData.address });
+
+    return res.status(200).json({
+      error: false,
+      message: "Password has been reset",
+      userId: userId,
+      username: userData.username,
+      email: userData.email
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error,
+      message: "Failed to reset password"
+    });
+  }
 });
-  
+
 module.exports = router;
