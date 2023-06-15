cb570deb7da7a6cca7c4fc01ac3270860593677f
alifmaulidanar <alifdanar1145@gmail.com>
2023-06-15 19:25:54 +0700

    Fix bugs in ML model API

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from tensorflow.keras.models import load_model
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import datetime
+import locale
 
 # Initialize Firebase credentials and Firestore
 cred = credentials.Certificate("./serviceAccountKey.json")
@@ -32,8 +34,12 @@ class InputData(BaseModel):
 
 # Define the output data schema
 class OutputData(BaseModel):
+    userId: str
     diagnosisId: str
+    test_type: str
+    date: str
     prediction: int
+    prediction_result: str
 
 # Load the ML model
 model = load_model('./elaborate_model.h5')
@@ -54,10 +60,40 @@ def get_data(diagnosisId: str, userId: str):
             if diagnosis.exists:
                 output_data = diagnosis.to_dict().get('output_data')
                 prediction = output_data.get('prediction')
+                prediction_result = output_data.get('prediction_result')
+                input_data_ordered = diagnosis.to_dict().get('input_data')
+                input_data = {
+                    'age': input_data_ordered.get('age'),
+                    'sex': input_data_ordered.get('sex'),
+                    'rbc': input_data_ordered.get('rbc'),
+                    'hgb': input_data_ordered.get('hgb'),
+                    'hct': input_data_ordered.get('hct'),
+                    'mcv': input_data_ordered.get('mcv'),
+                    'mch': input_data_ordered.get('mch'),
+                    'mchc': input_data_ordered.get('mchc'),
+                    'rdw_cv': input_data_ordered.get('rdw_cv'),
+                    'wbc': input_data_ordered.get('wbc'),
+                    'neu': input_data_ordered.get('neu'),
+                    'lym': input_data_ordered.get('lym'),
+                    'mo': input_data_ordered.get('mo'),
+                    'eos': input_data_ordered.get('eos'),
+                    'ba': input_data_ordered.get('ba')
+                }
                 diagnosis_data = {
-                    'diagnosisId': diagnosis.to_dict().get('diagnosisId'),
-                    'input_data': diagnosis.to_dict().get('input_data'),
-                    'prediction': prediction
+                    'userData': {
+                        'userId': user.to_dict().get('userId'),
+                        'username': user.to_dict().get('username'),
+                    },
+                    'diagnosisData': {
+                        'diagnosisId': diagnosis.to_dict().get('diagnosisId'),
+                        'test_type': diagnosis.to_dict().get('test_type'),
+                        'date': diagnosis.to_dict().get('date'),
+                        'input_data': input_data,
+                        'output_data': {
+                            'prediction': prediction,
+                            'prediction_result': prediction_result
+                        }
+                    }
                 }
                 return diagnosis_data
             else:
@@ -86,18 +122,54 @@ def predict(data: InputData, userId: str):
 
     # Convert the prediction to the corresponding class label
     class_label = np.argmax(prediction, axis=1)[0]
+    
+    # Define prediction_result based on class_label
+    if class_label == 0:
+        prediction_result = "Normal health status: no immediate need for medical consultation"
+    elif class_label == 1:
+        prediction_result = "It is advisable to seek a medical consultation in order to facilitate a comprehensive assessment for further diagnostic purposes"
+    elif class_label == 2:
+        prediction_result = "Medical consultation recommended for moderate condition"
+    elif class_label == 3:
+        prediction_result = "Immediate medical consultation required"
+    else:
+        prediction_result = "Unknown prediction value"
 
     # Generate diagnosisId
     diagnosisId = db.collection('diagnosis').document().id
     
-    # Create the output data
-    output_data = OutputData(diagnosisId=diagnosisId, prediction=class_label)
+    test_type = "Blood Test"
+    
+    # Set locale to Indonesian
+    locale.setlocale(locale.LC_TIME, 'id_ID')
+    
+    # Get today date
+    current_date = datetime.date.today()
 
+    # Create date format
+    current_date_str = current_date.strftime("%A, %d %B %Y")
+
+    # Create the output data
+    output_data = OutputData(
+        userId=userId,
+        diagnosisId=diagnosisId,
+        test_type=test_type,
+        date=current_date_str,
+        prediction=class_label,
+        prediction_result=prediction_result
+    )
+    
     # Save the input and output data to Firestore
     data_dict = {
+        'userId': userId,
         'diagnosisId': diagnosisId,
+        'test_type' : test_type,
+        'date': current_date_str,
         'input_data': data.dict(),
-        'output_data': output_data.dict()
+        'output_data': {
+            'prediction': output_data.prediction,
+            'prediction_result': output_data.prediction_result
+        }
     }
     doc_ref = db.collection('diagnosis').document(diagnosisId)
     doc_ref.set(data_dict)
